0c199566d731870599a325af8455f1c2bc667451
MarianCristianStan <marian.cristian.stan11@gmail.com>
2024-07-12 23:20:36 +0300

    refactor program.cs and add folders for arhitecture

--- sekhmet-server/Program.cs
+++ sekhmet-server/Program.cs
@@ -1,25 +1,37 @@
+
+using sekhmet_server;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+ConfigServices();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+RunApp();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+void ConfigServices()
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	builder.Services.AddControllers();
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	builder.Services.AddRepositories();
+	builder.Services.AddServices();
 }
 
-app.UseHttpsRedirection();
+void RunApp()
+{
+	var app = builder.Build();
 
-app.UseAuthorization();
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
 
-app.MapControllers();
+	app.UseHttpsRedirection();
 
-app.Run();
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+}
