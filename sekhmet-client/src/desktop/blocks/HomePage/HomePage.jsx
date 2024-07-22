09372504c66f7bfed8270c7c6cdeab81429aa01c
AlinBivolan <alinall0035@gmail.com>
2024-07-22 14:03:56 +0100

    Update - Frontend - SEK8

--- sekhmet-client/src/desktop/blocks/HomePage/HomePage.jsx
+++ sekhmet-client/src/desktop/blocks/HomePage/HomePage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-import bellImg from "../../../core/assets/icons/Bell.svg";
+import bellImg from "../../../core/assets/icons/NotificationsIcon.svg";
 import searchIconImg from "../../../core/assets/icons/SearchIcon.svg";
+import messageCircle from "../../../core/assets/icons/MessageIcon.svg";
 
 import styles from "./style.module.scss";
 import VideoPost from "../../components/VideoPost";
@@ -11,22 +12,22 @@ const HomePage = () => {
     <>
       <div className={styles.containerNavBar}>
         <div className={styles.wrapStart}>
-          <div className={styles.burger}>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
-            <div className={styles.line}></div>
-          </div>
+        <img
+            className={styles.bell}
+            src={messageCircle}
+            alt="Message notification icon"
+          />
           <p className={styles.title}>Sekhmet</p>
         </div>
         <div className={styles.searchBar}>
           <input className={styles.inputSearchBar}></input>
-          <button className={styles.searchButton}>
+          <div className={styles.searchButton}>
             <img
               className={styles.searchIconImg}
               src={searchIconImg}
               alt="Bell notification icon"
             />
-          </button>
+          </div>
         </div>
         <div className={styles.wrapEnd}>
           <img
@@ -40,10 +41,10 @@ const HomePage = () => {
       <div className={styles.content}>
         <div className={styles.filterOption}>
           <p className={styles.filterText1}>Following</p>
+          <p className={styles.filterText2}>Story</p>
           <p className={styles.filterText2}>Explore</p>
         </div>
         <div className={styles.filterUnderLine} />
-        <button className={styles.createButton}>Create</button>
       </div>
       <VideoPost />
       <VideoPost />
